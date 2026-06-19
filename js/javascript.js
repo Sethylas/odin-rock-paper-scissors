413877c7b0130210b796b51e1cabff8f2860d1c6
Sethylas <60220827+Sethylas@users.noreply.github.com>
2026-06-19 12:30:16 +0800

    Completed game logic, implementing: - Human choice to a digit 1-3, making str input uppercase - playRound which handles number comparisons and game logic - Moved human and computer choice down to prompt after function init - playGame which handles the round managements

--- js/javascript.js
+++ js/javascript.js
@@ -1,24 +1,89 @@
 let humanScore = 0;
-    computerScore = 0;
-    humanChoice = 0;
-    computerChoice = 0;
-
+let computerScore = 0;
 
 function getRandomNumber() {
     return Math.floor(Math.random() * (4 - 1) + 1);
 }
 
 function getComputerChoice() {
-    computerChoice = getRandomNumber();
-    return computerChoice;
+    let computerSelection = getRandomNumber();
+    return computerSelection;
 }
 
 function getHumanChoice() {
-    humanChoice = prompt("Please enter ROCK, PAPER or SCISSORS", "");
-    humanChoice = humanChoice.toUpperCase();
-    return humanChoice;
+    let humanSelection = prompt("Please enter ROCK, PAPER or SCISSORS", "");
+    humanSelection = humanSelection.toUpperCase();
+
+    switch (humanSelection) 
+    {
+        case 'ROCK':
+            humanSelection = 1;
+            break;
+        case 'PAPER':
+            humanSelection = 2;
+            break;
+        case 'SCISSORS':
+            humanSelection = 3;
+            break;
+        default:
+            humanSelection = 0;
+            break;
+    }
+    return humanSelection;
 }
 
+function playRound(humanChoice, computerChoice) {
+    if (humanChoice == computerChoice) {
+        console.log("Tie!");
+    }
 
-console.log(getHumanChoice());
-    
+    // PLAYER WIN
+    else if (humanChoice == 1 && computerChoice == 3) {
+        console.log("You win! Rock beats scissors.");
+        humanScore++;
+    }
+    else if (humanChoice == 2 && computerChoice == 1) {
+        console.log("You win! Paper beats rock.");
+        humanScore++;
+    }
+    else if (humanChoice == 3 && computerChoice == 2) {
+        console.log("You win! Scissors beats paper");
+        humanScore++;
+    }
+
+    // COMPUTER WIN
+    else if (computerChoice == 1 && humanChoice == 3) {
+        console.log("You Lose! Rock beats scissors.");
+        computerScore++;
+    }
+    else if (computerChoice == 2 && humanChoice == 1) {
+        console.log("You Lose! Paper beats rock.");
+        computerScore++;
+    }
+    else if (computerChoice == 3 && humanChoice == 2) {
+        console.log("You Lose! Scissors beats paper");
+        computerScore++;
+    }
+}
+
+let humanChoice = getHumanChoice();
+let computerChoice = getComputerChoice();
+
+function playGame() {
+    while(humanScore != 5 && computerScore != 5) 
+    {
+        playRound(humanChoice, computerChoice);
+        humanChoice = getHumanChoice();
+        computerChoice = getComputerChoice();
+    }
+
+    if (humanScore == 5) {
+        console.log("Congratulations, you win!");
+    }
+    else {
+        console.log("You lost!");
+    }
+
+}
+
+playGame();

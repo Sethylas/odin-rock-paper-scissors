eabf51b72f8f2759b1d83a092163b4b5d4084c26
Sethylas <60220827+Sethylas@users.noreply.github.com>
2026-06-19 12:12:46 +0800

    Added computer and human choice of input.
    Created a function to generate a random number
    Made html file not empty as personal preference.

--- js/javascript.js
+++ js/javascript.js
@@ -1,1 +1,24 @@
-console.log("Hello World");
+let humanScore = 0;
+    computerScore = 0;
+    humanChoice = 0;
+    computerChoice = 0;
+
+
+function getRandomNumber() {
+    return Math.floor(Math.random() * (4 - 1) + 1);
+}
+
+function getComputerChoice() {
+    computerChoice = getRandomNumber();
+    return computerChoice;
+}
+
+function getHumanChoice() {
+    humanChoice = prompt("Please enter ROCK, PAPER or SCISSORS", "");
+    humanChoice = humanChoice.toUpperCase();
+    return humanChoice;
+}
+
+
+console.log(getHumanChoice());
+    
